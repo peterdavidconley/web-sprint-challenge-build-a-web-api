92c02e35c3b51e1c5dba05b2235a93d57d43cca3
Peter Conley <peterdavidconley@gmail.com>
2022-02-11 09:15:44 -0500

    wrote index.js file

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,2 @@
 // Write your "actions" router here!
+
--- api/server.js
+++ api/server.js
@@ -2,8 +2,25 @@ const express = require('express');
 const server = express();
 
 // Configure your server here
+
+server.use(express.json());
+
 // Build your actions router in /api/actions/actions-router.js
+
+server.use('/api/users', (req, res) => {
+
+})
+
 // Build your projects router in /api/projects/projects-router.js
+
+server.use('/api/users', (req, res) => {
+
+})
+
 // Do NOT `server.listen()` inside this file!
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`);
+  });
 
 module.exports = server;
